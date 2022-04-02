a204edf92900b1cb42264e2bc735b642d69588ec
nhâm nguyễn <nhamqs2002@gmail.com>
2022-04-02 20:33:05 +0700

    fixx code

--- QUANLIQUANCAFE/QUANLIQUANCAFE/Form1.Designer.cs
+++ QUANLIQUANCAFE/QUANLIQUANCAFE/Form1.Designer.cs
@@ -31,16 +31,13 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+  
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
     }
 }
 
