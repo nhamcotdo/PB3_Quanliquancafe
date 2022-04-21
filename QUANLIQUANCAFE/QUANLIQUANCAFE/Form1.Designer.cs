d58beddb8ca390174caaa63e88fa3581050581d5
nhâm nguyễn <nhamqs2002@gmail.com>
2022-04-21 09:43:27 +0700

    Chia khu vuc code

--- QUANLIQUANCAFE/QUANLIQUANCAFE/Form1.Designer.cs
+++ QUANLIQUANCAFE/QUANLIQUANCAFE/Form1.Designer.cs
@@ -73,7 +73,7 @@
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(3, 10);
